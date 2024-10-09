70e1749a5fa061c68320e3c5857c54ff399c9f7d
sanjaisy711 <sanjaip711@gmail.com>
2024-10-09 11:33:33 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -37,11 +37,11 @@ export default function Page() {
             <div className="md:col-span-6">
               <div className="md:me-8">
                 <h4 className="font-bold lg:leading-normal leading-normal text-4xl lg:text-5xl mb-5 text-black dark:text-white relative">
-                Build smarter{" "}
+                Build Smarter{" "}
                   <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-indigo-600 relative inline-block">
                     <span className="relative text-white">&</span>
                   </span>{" "}
-                  Deploy faster
+                  Deploy Faster
                 </h4>
                 <p className="text-slate-400 text-lg max-w-xl">
                 Worksbyte lets you transform ideas into powerful Enterprise applications that adapt to your business needs. One platform. No coding. Completely hassle-free.
