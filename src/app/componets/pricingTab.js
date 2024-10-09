a3a7b3cec230676fec9233632fed1a1e29da2590
sanjaisy711 <sanjaip711@gmail.com>
2024-10-09 12:01:26 +0530

    Update pricingTab.js

--- src/app/componets/pricingTab.js
+++ src/app/componets/pricingTab.js
@@ -39,6 +39,21 @@ export default function PricingTab(){
                         {isOpenTab === 0 ?
                             <div>
                                 <div className="grid lg:grid-cols-4 md:grid-cols-2 mt-8 gap-[30px]">
+                                        <div className="group border-b-[3px] dark:border-gray-700 p-6 py-8 hover:border-indigo-600 dark:hover:border-indigo-600 transition-all duration-500 ease-in-out hover:scale-105 relative overflow-hidden shadow dark:shadow-gray-800 rounded-md bg-gray-50 dark:bg-slate-800 hover:bg-white dark:hover:bg-slate-900 h-fit">
+                                        <h6 className="font-bold uppercase mb-5 text-indigo-600">Free</h6>
+
+                                        <div className="flex mb-5">
+                                            <span className="text-xl font-semibold"></span>
+                                            <span className="price text-4xl font-semibold mb-0">Free Trail</span>
+                                            <span className="text-xl font-semibold self-end mb-1"></span>
+                                        </div>
+
+                                        <ul className="list-none text-slate-400">
+                                            <li className="mb-1 flex items-center"><BsCheckCircle className="text-indigo-600 text-base me-2"/>Design custom apps</li>
+                                            <li className="mb-1 flex items-center"><BsCheckCircle className="text-indigo-600 text-base me-2"/>Automate & digitise your business</li>
+                                        </ul>
+                                        <Link href="#" className="py-2 px-5 inline-block font-semibold tracking-wide border align-middle duration-500 text-base text-center bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white rounded-md mt-5">Schedule a Demo</Link>
+                                    </div>
                                     <div className="group border-b-[3px] dark:border-gray-700 p-6 py-8 hover:border-indigo-600 dark:hover:border-indigo-600 transition-all duration-500 ease-in-out hover:scale-105 relative overflow-hidden shadow dark:shadow-gray-800 rounded-md bg-gray-50 dark:bg-slate-800 hover:bg-white dark:hover:bg-slate-900 h-fit">
                                         <h6 className="font-bold uppercase mb-5 text-indigo-600">Gold</h6>
 
@@ -94,6 +109,21 @@ export default function PricingTab(){
                             {isOpenTab === 1 ?
                             <div >
                                 <div className="grid lg:grid-cols-4 md:grid-cols-2 mt-8 gap-[30px]">
+                                <div className="group border-b-[3px] dark:border-gray-700 p-6 py-8 hover:border-indigo-600 dark:hover:border-indigo-600 transition-all duration-500 ease-in-out hover:scale-105 relative overflow-hidden shadow dark:shadow-gray-800 rounded-md bg-gray-50 dark:bg-slate-800 hover:bg-white dark:hover:bg-slate-900 h-fit">
+                                        <h6 className="font-bold uppercase mb-5 text-indigo-600">Free</h6>
+
+                                        <div className="flex mb-5">
+                                            <span className="text-xl font-semibold"></span>
+                                            <span className="price text-4xl font-semibold mb-0">Free Trail</span>
+                                            <span className="text-xl font-semibold self-end mb-1"></span>
+                                        </div>
+
+                                        <ul className="list-none text-slate-400">
+                                            <li className="mb-1 flex items-center"><BsCheckCircle className="text-indigo-600 text-base me-2"/>Design custom apps</li>
+                                            <li className="mb-1 flex items-center"><BsCheckCircle className="text-indigo-600 text-base me-2"/>Automate & digitise your business</li>
+                                        </ul>
+                                        <Link href="#" className="py-2 px-5 inline-block font-semibold tracking-wide border align-middle duration-500 text-base text-center bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white rounded-md mt-5">Schedule a Demo</Link>
+                                    </div>
 
                                     <div className="group border-b-[3px] dark:border-gray-700 p-6 py-8 hover:border-indigo-600 dark:hover:border-indigo-600 transition-all duration-500 ease-in-out hover:scale-105 relative overflow-hidden shadow dark:shadow-gray-800 rounded-md bg-gray-50 dark:bg-slate-800 hover:bg-white dark:hover:bg-slate-900 h-fit">
                                         <h6 className="font-bold uppercase mb-5 text-indigo-600">Gold</h6>
