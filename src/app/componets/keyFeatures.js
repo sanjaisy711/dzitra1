25bcb2d079b254ecba28d026312b82d8340c9efa
sanjaisy711 <sanjaip711@gmail.com>
2024-10-09 12:44:24 +0530

    Update keyFeatures.js

--- src/app/componets/keyFeatures.js
+++ src/app/componets/keyFeatures.js
@@ -9,8 +9,6 @@ export default function Dzitra({btnFill}){
         <div className="container ">
             <div className="grid grid-cols-1 pb-8 text-center">
                 <h3 className="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold">Key Features</h3>
-
-                <p className="text-slate-400 max-w-xl mx-auto">Start working with Dzitra that can provide everything you need to generate awareness, drive traffic, connect.</p>
             </div>
 
             <div className="grid md:grid-cols-12 grid-cols-1 gap-[30px] mt-8">
