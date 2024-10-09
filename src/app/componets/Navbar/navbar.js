f40b059718ea82385063d4ef36f126b47e1497d5
sanjaisy711 <sanjaip711@gmail.com>
2024-10-09 18:04:23 +0530

    Update navbar.js

--- src/app/componets/Navbar/navbar.js
+++ src/app/componets/Navbar/navbar.js
@@ -68,14 +68,14 @@ export default function Navbar(props) {
                     <Link className="logo" href="/index">
                         <span className="inline-block dark:hidden">
                             <Image src='/images/logo-dark.png' className="l-dark" width={300} height={100}  alt="" />
-                            <Image src='/images/logo-light.png' className="l-light" width={138} height={24} alt="" />
+                            <Image src='/images/logo-light.png' className="l-light" width={300} height={100} alt="" />
                         </span>
-                        <Image src='/images/logo-light.png' width={250} height={100} className="hidden dark:inline-block" alt="" />
+                        <Image src='/images/logo-light.png' width={300} height={100} className="hidden dark:inline-block" alt="" />
                     </Link>
                     :
                     <Link className="logo" href="#">
                         <Image src='/images/logo-dark.png' width={300} height={100} className="inline-block dark:hidden" alt="" />
-                        <Image src='/images/logo-light.png' width={250} height={100} className="hidden dark:inline-block" alt="" />
+                        <Image src='/images/logo-light.png' width={300} height={100} className="hidden dark:inline-block" alt="" />
                     </Link>
             }
 
