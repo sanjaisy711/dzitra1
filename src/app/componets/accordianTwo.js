11bb0ca861f1001cae28a836741943e2e509f0fb
sanjaisy711 <sanjaip711@gmail.com>
2024-10-11 18:23:21 +0530

    Update accordianTwo.js

--- src/app/componets/accordianTwo.js
+++ src/app/componets/accordianTwo.js
@@ -1,6 +1,6 @@
 "use client"
 import React,{useState} from "react"
-import { accordionData1 } from "../Data/dataTwo";
+import { accordionData } from "../Data/dataTwo";
 
 export default function AccordionTwo(){
     let [activeIndex, setActiveIndex] = useState(0);
@@ -16,7 +16,7 @@ export default function AccordionTwo(){
         <>
         <div className="md:col-span-6">
             <div id="accordion-collapse">
-                {accordionData1.slice(0,8).map((item, index) => (
+                {accordionData.slice(0,8).map((item, index) => (
                     <div key={index} className="relative shadow dark:shadow-gray-800 rounded-md overflow-hidden mt-4">
                         <h2 className="text-base font-semibold" id="accordion-collapse-heading-1">
                             <button type="button" onClick={() => toggleAccordion(index)} className={`flex justify-between items-center p-5 w-full font-medium text-start ${activeIndex === index ? 'bg-gray-50 dark:bg-slate-800 text-indigo-600' : ''}`} data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
