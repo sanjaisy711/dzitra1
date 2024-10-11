6ea93353e1843fa8afd33fe89d2d460e31cc4952
sanjaisy711 <sanjaip711@gmail.com>
2024-10-11 19:18:02 +0530

    Update accordianTwo.js

--- src/app/componets/accordianTwo.js
+++ src/app/componets/accordianTwo.js
@@ -34,11 +34,12 @@ export default function AccordionTwo(){
                                         )}
                                     </div>
                                 ))}
+                                    <div className="mt-4">
+                                <Link href="/helpcenter-faqs" className="relative inline-flex items-center font-semibold tracking-wide align-middle text-base text-center border-none after:content-[''] after:absolute after:h-px after:w-0 hover:after:w-full after:end-0 hover:after:end-auto after:bottom-0 after:start-0 after:duration-500 text-indigo-600 hover:text-indigo-600 after:bg-indigo-600 duration-500">see more <MdKeyboardArrowRight className="text-xl ms-1"/></Link>
+                            </div>
                             </div>
                                 
         </>
     )
-                              <div className="mt-4">
-                                <Link href="/helpcenter-faqs" className="relative inline-flex items-center font-semibold tracking-wide align-middle text-base text-center border-none after:content-[''] after:absolute after:h-px after:w-0 hover:after:w-full after:end-0 hover:after:end-auto after:bottom-0 after:start-0 after:duration-500 text-indigo-600 hover:text-indigo-600 after:bg-indigo-600 duration-500">see more <MdKeyboardArrowRight className="text-xl ms-1"/></Link>
-                            </div>
+                              
 }
