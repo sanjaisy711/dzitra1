2353f4f1aeb13eb49d08b5b1b2f61cdd8e68cdaf
sanjaisy711 <sanjaip711@gmail.com>
2024-10-11 19:00:46 +0530

    Update accordianTwo.js

--- src/app/componets/accordianTwo.js
+++ src/app/componets/accordianTwo.js
@@ -19,10 +19,10 @@ export default function AccordionTwo(){
                                     <div key={index} className="relative shadow dark:shadow-gray-800 rounded-md overflow-hidden mt-4">
                                         <h2 className="text-base font-semibold" id="accordion-collapse-heading-1">
                                             <button onClick={() => toggleAccordion('active', index)} type="button"
-                                                className={`${state.activeIndex === index ? 'flex justify-between items-center p-5 w-full font-medium text-start bg-gray-50 dark:bg-slate-800 text-indigo-600' : 'flex justify-between items-center p-5 w-full font-medium text-start text-dark dark:text-white'}`}
+                                                className={`${activeIndex === index ? 'flex justify-between items-center p-5 w-full font-medium text-start bg-gray-50 dark:bg-slate-800 text-indigo-600' : 'flex justify-between items-center p-5 w-full font-medium text-start text-dark dark:text-white'}`}
                                     >
                                                 <span>{item.title}</span>
-                                                <svg data-accordion-icon className={`${state.activeIndex === index ? "rotate-180" : "rotate-270" } size-4 shrink-01`}  fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                <svg data-accordion-icon className={`${activeIndex === index ? "rotate-180" : "rotate-270" } size-4 shrink-01`}  fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                     <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path>
                                                 </svg>
                                             </button>
