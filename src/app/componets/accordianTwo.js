3e6c11851a860638d9633b3f1960611efe745f33
sanjaisy711 <sanjaip711@gmail.com>
2024-10-11 18:56:14 +0530

    Update accordianTwo.js

--- src/app/componets/accordianTwo.js
+++ src/app/componets/accordianTwo.js
@@ -37,7 +37,6 @@ export default function AccordionTwo(){
                                     </div>
                                 ))}
                             </div>
-        </div>
         </>
     )
 }
